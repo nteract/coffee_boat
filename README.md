2dd283602350aa9267dfcfeb992921c76c9f55a0
Holden Karau <holdenkarau@google.com>
2018-01-08 19:33:32 -0800

    Try and add some tests

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pyspark-deps
 WIP PySpark dependency management.
+
+## Tests
+
+Running tests will modify your current virtualenv's packages. This software is WIP.
+
+## Using
+
+Oh god no
--- coffee_boat/tests.py
+++ coffee_boat/tests.py
@@ -2,25 +2,58 @@ import pyspark
 import unittest2
 
 class TestBasicDep(unittest2.TestCase):
+  """Test we package dependencies. Note: this test case is EVIL!"""
+
   def test_simple_env(self):
     from coffee_boat import Captain
     # Creat a captain
-    captain = Captain()
+    captain = Captain(accept_conda_license=True)
     # Validate we don't have nbconvert installed in local context
-    def fail_at_import():
+    import subprocess
+    subprocess.call(["pip", "uninstall", "-y", "nbconvert"])
+    with self.assertRaises(ImportError):
       import nbconvert
-    assertRaises("ImportError", fail_at_import)
     captain.add_pip_packages("pandas==0.22.0","nbconvert")
     # We should now have it....
     import nbconvert
     import pandas
     captain.launch_ship()
-    sc = pyspark.context.getOrCreate()
-    rdd = sc.parallelize(range(100))
+    sc = pyspark.context.SparkContext.getOrCreate()
+    rdd = sc.parallelize(range(2))
     def find_info(x):
       import os
       import nbconvert
-      return (x, os.environ['PYTHONPATH'])
-    result = rdd.map(lambda x: find_info)
-    print(result)
-    assert(result == [])
+      import sys
+      return (x, sys.executable, os.environ['PYTHONPATH'], os.listdir('.'))
+    result = rdd.map(find_info).collect()
+    print("RESULT IS {0}".format(result))
+    sc.stop()
+    self.assertEqual(result, [])
+
+
+  def boop_non_local_env(self):
+    from coffee_boat import Captain
+    # Creat a captain
+    captain = Captain(install_local=False, accept_conda_license=True)
+    # Validate we don't have nbconvert installed in local context
+    import subprocess
+    subprocess.call(["pip", "uninstall", "-y", "nbconvert"])
+    with self.assertRaises(ImportError):
+      import nbconvert
+    captain.add_pip_packages("pandas==0.22.0","nbconvert")
+    # We should now have it....
+    import nbconvert
+    import pandas
+    captain.launch_ship()
+    sc = pyspark.context.SparkContext.getOrCreate()
+    rdd = sc.parallelize(range(2))
+    def find_info(x):
+      import os
+      import nbconvert
+      import sys
+      return (x, sys.executable, os.environ['PYTHONPATH'])
+    result = rdd.map(find_info).collect()
+    print("RESULT IS {0}".format(result))
+    self.assertEqual(result, [])
+    with self.assertRaises(ImportError):
+      import nbconvert
