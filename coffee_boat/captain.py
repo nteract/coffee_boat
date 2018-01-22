3a3022feb38bd1637503dcf2eb51ee1c0c9b0b76
Holden Karau <holdenkarau@google.com>
2018-01-21 19:18:38 -0800

    Just use a single log file

--- coffee_boat/captain.py
+++ coffee_boat/captain.py
@@ -212,18 +212,21 @@ class Captain(object):
         touch setup.lock
         if [ -f {0} ];
         then
-            unzip {0} &>/dev/null && rm {0} &>> setup_log.txt
+            echo "Running setup" >> coffee_log.txt
+            unzip {0} &>/dev/null && rm {0} &>> coffee_log.txt
             # Since Conda isn't really fully relocatable...
-            mv {1} {1}_src &>> setup_log.txt
-            rm -rf ./coffee_boat_conda &>> setup_log.txt
+            mv {1} {1}_src &>> coffee_log.txt
+            rm -rf ./coffee_boat_conda &>> coffee_log.txt
             # TODO: avoid clone if we don't need it (non-dynamic install)
-            {1}_src/bin/conda create --prefix ./coffee_boat_conda --clone {1}_src &>> setup_log.txt
+            {1}_src/bin/conda create --prefix ./coffee_boat_conda --clone {1}_src &>> coffee_log.txt
         fi
-        source ./coffee_boat_conda/bin/activate &>> activate_log.txt
+        echo "Activating env..." >> coffee_log.txt
+        source ./coffee_boat_conda/bin/activate &>> coffee_log.txt
         cat magicCoffeeReq* > mini_req.txt &> /dev/null || true
-        ./coffee_boat_conda/bin/pip install -r mini_req.txt &>> pip_install_log.txt
+        echo "pip install" >> coffee_log.txt
+        ./coffee_boat_conda/bin/pip install -r mini_req.txt &>> coffee_log.txt
         export PATH=./coffee_boat_conda/bin:$PATH
-        ./coffee_boat_conda/bin/python "$@" || cat *.txt 1>&2 """.format(zip_name, relative_conda_path))
+        ./coffee_boat_conda/bin/python "$@" || cat coffee_log.txt 1>&2 """.format(zip_name, relative_conda_path))
         script_name = "coffee_boat_runner_{0}.sh".format(self.env_name)
         runner_script_path = os.path.join(self.working_dir, script_name)
         with open(runner_script_path, 'w') as f:
